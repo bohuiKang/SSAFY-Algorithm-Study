2cf3ca0d4a1ed05975e9217cfba15a47582d7edd
bohui <lodin356@naver.com>
2026-01-09 19:07:21 +0900

    update

--- 2601_january/jan_week1/bruteforce2_prime/bh_bruteforce2.py
+++ 2601_january/jan_week1/bruteforce2_prime/bh_bruteforce2.py
@@ -4,35 +4,35 @@
 import math
 
 def solution(numbers):
-    making_n = set() # 숫자 조합을 저장할 변수
+    making_n = set()                # 숫자 조합을 저장할 변수
     answer = 0
 
     # 1. 숫자 조합 만드는 내부 함
     def make_number(numbers, idx_arr, making): 
-        making_n.add(int(making)) # 만들어진 조합 set에 저장
+        making_n.add(int(making))   # 만들어진 조합 set에 저장
 
-        if len(making) == len(numbers): # 만들어진 숫자가 제공된 숫자의 길이와 같을 때,
+        if len(making) == len(numbers):     # 만들어진 숫자가 제공된 숫자의 길이와 같을 때,
             return
         
         for i in range(len(numbers)):
-            if i in idx_arr: # 조합시 내 인덱스는 제외
+            if i in idx_arr:         # 조합시 내 인덱스는 제외
                 continue
             make_number(numbers, idx_arr+[i], making+numbers[i])
         return
     
     # 2. 소수 찾는 함수
     def check_prime(number): 
-        divisor = int(math.sqrt(number)) # 제곱근 값 구하기
+        divisor = int(math.sqrt(number))    # 제곱근 값 구하기
         
         for n in range(2, divisor + 1):
             if number % n == 0:
                 return 0
-        return 1 # 나누어 지는 값이 없으므로 number은 소수!
+        return 1                        # 나누어 지는 값이 없으므로 number은 소수!
 
-    for idx in range(len(numbers)): # 숫자 조합 함수 호출
-        make_number(numbers, [idx], numbers[idx]) # 숫자, 사용한 위치 값 리스트, 숫자
+    for idx in range(len(numbers)):     # 숫자 조합 함수 호출
+        make_number(numbers, [idx], numbers[idx])   # 숫자, 사용한 위치 값 리스트, 숫자
 
-    for n in making_n: # 소수 확인 함수 ghcnf
+    for n in making_n:                    # 소수 확인 함수 ghcnf
         if n > 1:
             answer += check_prime(n) 
     return answer
