cc1c7923c0ce8fcfdc871f174a8dd1c99235b589
ayeong <bay18200725@gmail.com>
2026-02-19 08:19:28 +0900

    test_ayeong

--- 2601_january/jan_week2/greedy3_island/ay_greedy3.py
+++ 2601_january/jan_week2/greedy3_island/ay_greedy3.py
@@ -12,15 +12,36 @@
 # n개 체크 리스트 만들고 -> 인덱스로 체크
 # costs -> costs 작은 순으로 정렬
 # 아직 리스트 1로 안 바뀌었으면 포함하도록 진행
-# 그런데 
+# 그런데 갑자기 생각났음!!
+# 이렇게 되면 이어진것을 판별을 못함 이어지는 길을 탐색을 해야함
+# 부모가 필요 - 오 생각한게 맞았네
+
+
 def solution(n, costs):
     answer = 0
     check_num = [0]*n
     # cost 기준으로 정렬
     costs.sort(key= lambda x: x[2])
+    parents = [0]*n
+    # 초기 부모 지정 - 자기자신
+    for i in range(n):
+        parents[i] = i
     
+    # cycle인지 체크하는 함수
+    def is_cycle(i):
+        # 부모가 자기 자신이면 종료
+        
+        while parents[i] != i:
+            
+            pass
+    
+    # 비용 확인하면서 순환하기
     for i, j, cost in costs:
-        pass
+        pre = parents[i]
+        parents[i] = j
+    
+    
+        
     
         
         
